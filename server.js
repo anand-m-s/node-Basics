6bf934dd866861fd581d0a719302f1d3d6f71c9b
anandms <anandsathyan19@gmail.com>
2023-09-06 19:55:27 +0530

    Here have code

--- server.js
+++ server.js
@@ -1,31 +1,45 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    // console.log(req.url, req.method);
+
+    //lodash
+
+    let num = _.random(0, 20);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log("Hello");
+    })
+
+    greet();
+    greet();
+
 
     //set header content type
     res.setHeader('Content-Type', 'text/html');
 
     let path = './views/';
-    switch(req.url){
+    switch (req.url) {
         case '/':
             path += 'index.html';
-            res.statusCode =200;
+            res.statusCode = 200;
             break;
-        case '/about' :
+        case '/about':
             path += 'about.html'
-            res.statusCode =200;
+            res.statusCode = 200;
             break;
-        case '/about-me' :
-                res.statusCode =301;
-                res.setHeader('Location','/about');
-                res.end()
-                break;
-        default : 
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end()
+            break;
+        default:
             path += '404.html';
-            res.statusCode =404;
-            break;      
+            res.statusCode = 404;
+            break;
     }
 
     //send an html file
